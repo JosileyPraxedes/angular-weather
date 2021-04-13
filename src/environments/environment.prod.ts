a00ed3737ee32bf4fd26f1f933275001c50e3ebd
Josiley Cesario <josileycpraxedes@gmail.com>
2021-04-13 13:21:09 -0300

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '33289a0005b1f8b83c68d295019f933b',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '33289a0005b1f8b83c68d295019f933b',
 };
 
 /*
